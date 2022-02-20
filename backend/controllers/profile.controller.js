c316979d100ae908a5a7fe89362f5faf9de070cc
iheb abbassi <92274939+ihebabbassi@users.noreply.github.com>
2022-02-21 00:59:37 +0100

    Update profile.controller.js

--- backend/controllers/profile.controller.js
+++ backend/controllers/profile.controller.js
@@ -3,6 +3,7 @@ const bcrypt=require('bcrypt');
 const User=require('./../models/trainer');
 const jwt=require('jsonwebtoken');
 const fs=require('fs');
+
 exports.current_user=(req,res)=>{
 	return res.status(200).send({
 		message:'Current user data successfully fetched',
