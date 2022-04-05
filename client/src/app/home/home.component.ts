ae24423a07e024b9181378d4f38e35b453ed5b12
jlassi14 <98310820+jlassi14@users.noreply.github.com>
2022-04-04 17:41:08 -0700

    trainer list front & back

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, ParamMap } from "@angular/router";
 import { Subscription } from "rxjs";
 import { AuthService } from "../auth/auth.service";
+import {MemberService} from "../member.service"
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -10,9 +12,10 @@ import { AuthService } from "../auth/auth.service";
 })
 
 export class HomeComponent implements OnInit {
+  trainers:any = [];
   userIsAuthenticated = false;
   private authListenerSubs: Subscription = new Subscription;
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,private memberService: MemberService,private router: Router) { }
 
   ngOnInit() {
     this.userIsAuthenticated = this.authService.getIsAuth();
@@ -20,5 +23,20 @@ export class HomeComponent implements OnInit {
       .getAuthStatusListener()
       .subscribe(isAuthenticated => {
         this.userIsAuthenticated = isAuthenticated;
-      });}
+      });
+      this.readListTrainers();
+    }
+
+    readListTrainers(){
+      this.memberService.getAllTrainers().subscribe((data) => {
+       this.trainers = data;
+       console.log("trainers",this.trainers)
+      })    
+    }
+
+    send(id : string) {
+      //console.log(email)
+        this.router.navigate(['trainerview', id]);
+      }
+
 }
